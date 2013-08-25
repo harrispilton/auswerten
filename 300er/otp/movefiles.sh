734395798dcf929e33033d5532fa64fc8657ee77
harrispilton <harris_pilton@gmx.de>
2013-08-25 15:58:35 +0200

    neues verzeichnis

--- 300er/otp/movefiles.sh
+++ 300er/otp/movefiles.sh
@@ -5,7 +5,6 @@ clear
 cp otp/1d/5.dat otp/1d/314.7_K.dat
 cp otp/1d/7.dat otp/1d/299.7_K.dat
 cp otp/1d/9.dat otp/1d/282.5_K.dat
-#mv otp/1d/12.dat otp/1d/273.8_K.dat
 cp otp/1d/14.dat otp/1d/267.5_K.dat
 cp otp/1d/16.dat otp/1d/260.0_K.dat
 cp otp/1d/20.dat otp/1d/246.6_K.dat
@@ -21,3 +20,33 @@ cp otp/1d/52.dat otp/1d/293.2_K.dat
 cp otp/1d/54.dat otp/1d/285.8_K.dat
 cp otp/1d/56.dat otp/1d/280.0_K.dat
 cp otp/1d/59.dat otp/1d/276.9_K.dat
+
+
+cp otp/4.dat otp/invrec/314.7_K.dat
+cp otp/6.dat otp/invrec/299.7_K.dat
+cp otp/3.dat otp/invrec/323.6_K.dat
+cp otp/8.dat otp/invrec/282.5_K.dat
+cp otp/10.dat otp/invrec/274.0_K.dat
+cp otp/44.dat otp/invrec/311.6_K.dat
+cp otp/46.dat otp/invrec/309.0_K.dat
+cp otp/48.dat otp/invrec/302.4_K.dat
+cp otp/51.dat otp/invrec/296.9_K.dat
+cp otp/53.dat otp/invrec/293.2_K.dat
+cp otp/55.dat otp/invrec/285.8_K.dat
+cp otp/57.dat otp/invrec/280.0_K.dat
+cp otp/60.dat otp/invrec/276.9_K.dat
+
+
+cp otp/13.dat otp/satrec/273.8_K.dat
+cp otp/15.dat otp/satrec/267.5_K.dat
+cp otp/19.dat otp/satrec/260.0_K.dat
+cp otp/29.dat otp/satrec/238.0_K.dat
+cp otp/37.dat otp/satrec/292.3_K.dat
+cp otp/35.dat otp/satrec/306.5_K.dat
+cp otp/41.dat otp/satrec/331.5_K.dat
+cp otp/58.dat otp/satrec/280.0_K.dat
+cp otp/61.dat otp/satrec/276.9_K.dat
+cp otp/65.dat otp/satrec/219.9_K.dat
+cp otp/66.dat otp/satrec/232.8_K.dat
+cp otp/67.dat otp/satrec/233.0_K.dat
+cp otp/68.dat otp/satrec/249.9_K.dat
