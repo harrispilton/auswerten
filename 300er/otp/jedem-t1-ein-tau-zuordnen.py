2bdd26c35ab4ed2160c803d9930b4e0850e659fe
harrispilton <harris_pilton@gmx.de>
2013-10-11 17:03:05 +0200

    nachtrag

---
+++ 300er/otp/jedem-t1-ein-tau-zuordnen.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+#import glob
+#import re
+import itertools
+import numpy as np
+#import scipy as sp
+#from scipy.optimize import curve_fit
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+#from matplotlib.widgets import Slider, Button, CheckButtons
+def J_debye(omega,tau):
+	return (tau/(1+(omega*tau)))
+def J_cd(omega,tau_cd,beta=1):
+	a=omega*tau_cd
+	return (np.sin(beta*np.arctan(a))/(omega*(1+a**2)**(beta/2.)))
+def R1_DD(omega,tau,K_dd,beta=1):
+	return K_dd*(J_cd(omega,tau,beta)+4*J_cd(2*omega,tau,beta))
+def R1_ges(omega,tau,K_dd,delta_sigma,beta=1):
+	return R1_DD(omega,tau,K_dd,beta)+R1_CSA(omega,tau,delta_sigma,beta)
+def Susz(omega,tau,K_dd,delta_sigma,beta=1):
+	return K_dd*(omega*J_cd(omega,tau,beta)+4*omega*J_cd(2*omega,tau,beta))+0.3*omega**3*delta_sigma**2*J_cd(omega,tau,beta)
+delta_sigma_ppm=220e-6# dies ist eine konstante groesse angegeben in ppm diese zahl bezieht sich auf nu = 47.262 MHz und kommt aus der Diplomarbeit von Daniel Bock Seite 28
+nu=np.logspace(1.5,12.2,120)
+nu=[121.49e6,40e6]
+K_DDs=np.logspace(1,10,5)
+plt.ion()
+plt.figure(1)
+rateax=plt.axes([0.15,0.1,0.80,0.8])
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel(r'$\tau [s]$')
+plt.ylabel(r'$T_1$')
+#alpha=np.linspace(0,1,10)
+tau=taurot=np.logspace(-12,-6,120)
+#x=np.linspace(0.01,10,1e3)
+#ax.plot(x,susz(x))
+#plt.draw()
+#plt.show()
+#i = raw_input('next')
+#for t in tau:
+#	ax.plot(nu,R1_ges(nu,t,K_DDs[1],delta_sigma_ppm),label='tau ='+str(t)+'K_DD = 1e'+str(np.log10(K_DDs[1])),alpha=(np.log10(K_DDs[1])-2)/10,color=colors.next())
+t1dd=[1/R1_DD(46.072e6*2*np.pi,t/0.5,3*np.pi**2/10*(182e3)**2,0.5) for t in tau]
+rateax.plot(tau,t1dd,label='nu = 46.072 MHz')
+t1dd=[1/R1_DD(55e6*2*np.pi,t/0.5,3*np.pi**2/10*(182e3)**2,0.5) for t in tau]
+rateax.plot(tau,t1dd,label='nu = 55 MHz')
+#	rateax.plot(nu,R1_CSA(nu,tau[i],delta_sigma_ppm),label='csa',alpha=0.4)
+#	rateax.plot(nu,R1_DD(nu,tau[i],K_DDs[1]),label='dd',alpha=0.4)
+#x=np.logspace(-3.0,1.5,200,10)
+#ax.plot(x,J_debye(x,1),label=r'$\tau =1$, $\beta=1$')
+#ax.plot(x,J_debye(x,2),label=r'$\tau =2$, $\beta=1$')
+#ax.plot(x,J_cd(x,5,0.2),label=r'$\tau =4$, $\beta=0.3$')
+rateax.legend()
+plt.show()
+
+#suszax.plot(tau,Susz(nu[1]*2*np.pi,tau,K_DDs[1],delta_sigma_ppm,1),color=c[i])
+i=raw_input('ente')
---
+++ 300er/otp/stack.py
@@ -0,0 +1,187 @@
+#!/usr/bin/python
+import glob
+import re
+import itertools
+import numpy as np
+#import scipy as sp
+#from scipy.optimize import curve_fit
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.collections import PolyCollection
+from matplotlib.colors import colorConverter
+from scipy.optimize import leastsq
+from lmfit import minimize, Parameters, report_errors
+#import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+#from matplotlib.widgets import Slider, Button, CheckButtons
+
+def residual(params,xdata,ydata=None):
+	'''residual(params,xdata,ydata=None), return residual of a lorentz'''
+	f0=params['f0'].value
+	hwhm=10**params['loghwhm'].value
+	a0=params['a0'].value
+	y0=params['y0'].value
+	if ydata is None:
+		return Lorentz(xdata,f0,hwhm,a0,y0)
+	return (ydata-Lorentz(xdata,f0,hwhm,a0,y0))
+def Lorentz(x,f0,hwhm,a0,y0=0):
+	'''Lorentz(x,f0,hwhm,a0,y0=0), return yvalue of a point of a lorentzian with corresponding properties'''
+	return (y0+a0/(1.+((x-f0)/hwhm)**2))
+	#return (a0 * (0.5 * fwhm)/((x * f0)**2.0 + 0.25 * fwhm**2))
+params = Parameters()
+params.add('loghwhm',value=2.0,min=-2.0,max=8.0)
+pi=np.pi
+params.add('t_2',expr='(1/(pi*10**(loghwhm)))')
+params.add('a0', value=1e-3, min=1e-9,max=4.1,vary=True)
+params.add('f0', value=0,min=-1e1,max=1e1)
+params.add('y0',value=0.015,min=0,max=0.2,vary=False)
+plt.ion()
+fig=plt.figure(1)
+ax=fig.gca(projection='3d')
+x=np.linspace(-10,10,1e3)
+cc=lambda arg: colorConverter.to_rgba(arg,alpha=0.6)
+markers = itertools.cycle(['o','s','v'])
+files=glob.glob('otp/solidecho/'+'*K.dat')
+#files=glob.glob('otp/solidecho/26'+'*K.dat')
+files.sort()
+### in fitgrenzen.dat stehen nur grenzen fuer temperaturen groesser 260K, unter dieser temperatur gibt es keine lorentzlinie mehr. daher ist darauf zu achten dass diese auch nicht eingelesen werden (2zeilen drueber. man kann die  files auskommentieren durch anhaengen 2er rauten im entsprechenden ordner.
+with open('otp/solidecho/fitgrenzen.dat','r') as grin:
+	freqmins=[]
+	freqmaxs=[]
+	lines=grin.readlines()
+	for line in lines:
+		liste=line.split()
+		freqmins.append(float(liste[1]))
+		freqmaxs.append(float(liste[2]))
+
+print type(x)
+p0=[2,1.0e4,1.0]
+#a0_a=[]
+#fwhm_a=[]
+#f0_a=[]
+#for freq in fr:
+#	a0_a.append(a0)
+#	fwhm_a.append(fwhm)
+#	f0_a.append(f0)
+freqs=[]
+betrags=[]
+temps=[]
+fittemps=[]
+t2s=[]
+areas=[]
+verts=[]
+fitverts=[]
+for (data,frmin,frmax) in zip(files,freqmins,freqmaxs):
+	#ax.cla()
+	f=open(data,'r')
+	lines=f.readlines()
+	for i in range(0,2): lines.pop(0)
+	freq=[]
+	betrag=[]
+	for line in lines:
+		liste=line.split()
+		freq.append(np.float(liste[0]))
+		betrag.append(((np.float(liste[1]))**2+(np.float(liste[2])**2))**0.5)
+	ar1=0.
+	for i in range(0,len(betrag)-1):
+		ar1=ar1+(betrag[i])#*(-freq[i+1]+freq[i]))
+	maxbetrag=max(betrag)
+	betrag=[b/ar1 for b in betrag]
+	maxfreq=max(freq)
+	freq=[f/maxfreq for f in freq]
+	freqs.append(freq)
+	betrags.append(betrag)
+	verts.append(list(zip(freq,betrag)))
+	temps.append(float(data[14:-6]))
+	#ax.plot(freq,betrag,label=str(data),marker=markers.next(),linestyle='None')	
+	plt.autoscale()
+	plt.draw()
+	plt.show()
+	#plt.plot(fr,Lorentz(fr,f0,fwhm,a0))
+	fitten='y'
+	if str(fitten)=='y':
+		params['loghwhm'].value=2.3
+		my0=0.
+		for i in range(-int(len(betrag)*0.04),int(len(betrag)*0.04)): 
+			my0=my0+betrag[i]
+		my0=my0/(2.*int(len(betrag)*0.04)+1.0)
+		print my0
+		params['y0'].value=my0
+		#for i in range(freq.__len__()-1,-1,-1):
+		#	if abs(freq[i])>1.5e4:
+		#		freq.pop(i)
+		l=len(freq)
+		freq2=[]
+		betrag2=[]
+		#unteregrenze=int(l/2.-(i/50.*l))
+		#oberegrenze=int(l/2.+(i/50.*l))
+		#print unteregrenze-oberegrenze
+		for fr, b in zip(freq,betrag):
+			if fr>frmin and fr<frmax:
+				freq2.append(fr)
+				betrag2.append(b)
+		#freq2=np.array(freq[unteregrenze:oberegrenze])
+		#betrag2=np.array(betrag[unteregrenze:oberegrenze])
+		out = minimize(residual, params,args=(freq2,betrag2))
+		result=freq2+out.residual
+		fit = residual(params, np.array(freq))
+		print report_errors(params)
+		#ax.plot(freq,fit,label=str(temps[-1])+' Fit')
+		plt.legend()
+		t2s.append(params['t_2'].value)
+		fittemps.append(temps[-1])
+		fitverts.append(list(zip(freq,fit)))
+	ar1=0.
+	ar2=0.
+	for i in range(0,len(betrag)-1):
+		ar1=ar1+((betrag[i]-params['y0'].value)*(freq[i+1]-freq[i]))
+		ar2=ar2+((fit[i]-params['y0'].value)*(freq[i+1]-freq[i]))
+#	for b,fr,fi in zip(betrag,freq,fit):
+#		ar1=ar1+(b*(fr-freq[-1+freq.pop(fr)]
+#		ar2=ar2+fi
+	areas.append(ar1/ar2)
+
+	
+	
+	#sdfo=raw_input('next')
+poly=PolyCollection(verts,facecolors=[cc('r'),cc('g'),cc('y')])
+poly.set_alpha(0.7)
+ax.add_collection3d(poly,zs=temps,zdir='y')
+ax.set_xlim3d(-1e0,1e0)
+ax.set_ylim3d(248,335)
+ax.set_zlim3d(0,1e-4)
+#plt.figure(3)
+#arax=plt.axes([0.1,0.1,0.8,0.8])
+#arax.plot(temps,areas)
+with open('otp_T2.dat','w') as fout: 
+	for i in range(0,t2s.__len__()):
+		fout.write(str(fittemps[i])+' '+str(t2s[i])+'\n')
+#	print type(freq), type(betrag), type(p0), type(p0[0])
+#	fitpars, covmat = curve_fit(
+#			Lorentz,
+#			freq,
+#			betrag,
+#			p0)
+#			#[f0,fwhm,a0],
+#			#maxfev=10000
+#			#)
+#	print 'fitparameter '+str(fitpars)+'\n\ncovmat '+str(covmat)+'\n\n\n'
+#	print "fitpars[0]"+str(fitpars[0])
+#	print "fitpars[0]"+str(fitpars[0])
+#	print "fitpars[0]"+str(fitpars[0])
+#	print "fitpars[0]"+str(fitpars[0])
+#	with open('otp_T1.dat','a') as fout:
+#		fout.write(str(data[7:10])+'\t'+str(np.pi/fitpars[1])+'\n')
+#	plt.plot(freq,Lorentz(freq,fitpars[0],fitpars[1],fitpars[2]),
+			#label=data[7:10]
+#			)
+#	p0=fitpars
+plt.legend()
+plt.show()
+
+
+plt.figure(2)
+plt.xscale('linear')
+plt.yscale('log')
+plt.plot(fittemps,t2s,linestyle='None',marker='x')
+plt.draw()
+i=raw_input('ente')
