2d2f592269bb8443ecfdaaca842393c6a293d60e
harrispilton <harris_pilton@gmx.de>
2013-11-28 18:59:51 +0100

    der jplot ist neu

--- ffc/Jplot.py
+++ ffc/Jplot.py
@@ -11,54 +11,86 @@ import matplotlib.pyplot as plt
 c=[]
 for i in np.arange(10):c.append(cm.jet(1-i/10.))
 colors =itertools.cycle(c)
-def Lorentz(x,f0,fwhm,a0):
-	return (a0 * (0.5 * fwhm)/((x * f0)**2.0 + 0.25 * fwhm**2))
 
-def J_debye(omega,tau):
-	return (tau/(1+(omega*tau)))
-def J_cd(omega,tau_cd,beta=1):
+def J_cd(omega,tau_cd,beta=1.):
 	a=omega*tau_cd
-	return (np.sin(beta*np.arctan(a))/(omega*(1+a**2)**(beta/2.)))
-def R1_CSA(omega,tau,delta_sigma,beta=1):
-	return 0.3*omega**2*delta_sigma**2*J_cd(omega,beta)
-def R1_DD(omega,tau,K_dd,beta=1):
-	return K_dd*(J_cd(omega,tau,beta)+4*J_cd(2*omega,tau,beta))
-def R1_ges(omega,tau,K_dd,delta_sigma,beta=1):
-	return R1_DD(omega,tau,K_dd,beta)+R1_CSA(omega,tau,delta_sigma,beta)
-def Susz(omega,tau,K_dd,delta_sigma,beta=1):
-	return K_dd*(omega*J_cd(omega,tau,beta)+4*omega*J_cd(2*omega,tau,beta))+0.3*omega**3*delta_sigma**2*J_cd(omega,tau,beta)
-delta_sigma_ppm=220e-6# dies ist eine konstante groesse angegeben in ppm diese zahl bezieht sich auf nu = 47.262 MHz und kommt aus der Diplomarbeit von Daniel Bock Seite 28
-nu=np.logspace(1.5,12.2,120)
-nu=[121.49e6,40e6]
-K_DDs=np.logspace(1,10,5)
-plt.ion()
-plt.figure(1)
-rateax=plt.axes([0.05,0.1,0.80,0.8])
-plt.xscale('log')
-plt.yscale('log')
-plt.figure(2)
-suszax=plt.axes([0.1,0.1,0.8,0.8])
-plt.xscale('log')
-plt.yscale('log')
-#alpha=np.linspace(0,1,10)
-tau=taurot=np.logspace(-11,-7,120)
-#x=np.linspace(0.01,10,1e3)
-#ax.plot(x,susz(x))
-#plt.draw()
-#plt.show()
-#i = raw_input('next')
-#for t in tau:
-#	ax.plot(nu,R1_ges(nu,t,K_DDs[1],delta_sigma_ppm),label='tau ='+str(t)+'K_DD = 1e'+str(np.log10(K_DDs[1])),alpha=(np.log10(K_DDs[1])-2)/10,color=colors.next())
-t1csa=[1/R1_CSA(nu[1]*2*np.pi,t/0.4,delta_sigma_ppm,0.4) for t in tau]
-rateax.plot(tau,t1csa,label='nu ='+str(nu[1])+'K_DD = 1e'+str(np.log10(K_DDs[4])),color=c[0])
-#	rateax.plot(nu,R1_CSA(nu,tau[i],delta_sigma_ppm),label='csa',alpha=0.4)
-#	rateax.plot(nu,R1_DD(nu,tau[i],K_DDs[1]),label='dd',alpha=0.4)
-#x=np.logspace(-3.0,1.5,200,10)
-#ax.plot(x,J_debye(x,1),label=r'$\tau =1$, $\beta=1$')
-#ax.plot(x,J_debye(x,2),label=r'$\tau =2$, $\beta=1$')
-#ax.plot(x,J_cd(x,5,0.2),label=r'$\tau =4$, $\beta=0.3$')
-rateax.legend()
-plt.show()
+	return (1/omega*(np.sin(beta*np.arctan(a)))/((1.+a**2.)**(beta/2.)))
+def R1_CSA(omega,tau,delta_sigma,beta=1.):
+	return 0.3*omega**2.*delta_sigma**2.*J_cd(omega,tau,beta)
+def R1_DD(omega,tau,k_dd,beta=1.):
+	return k_dd*(J_cd(omega,tau,beta)+4*J_cd(2*omega,tau,beta))
+def R1_ges(omega,tau,k_dd,delta_sigma,beta=1.):
+	return R1_DD(omega,tau,k_dd,beta)+R1_CSA(omega,tau,delta_sigma,beta)
 
-suszax.plot(tau,Susz(nu[1]*2*np.pi,tau,K_DDs[1],delta_sigma_ppm,1),color=c[i])
+
+plt.ion()
+
+plt.figure(1)
+rateax=plt.axes([0.1,0.1,0.8,0.8])
+plt.xscale('log')
+plt.yscale('log')
+plt.title('Einfluss von CSA bei verschiedenen Temperaturen')
+rateax.set_xlabel(r'$\nu  [MHz]$')
+rateax.set_ylabel(r'$R_1$')
+plt.ylim([1e-2,1e2])
+plt.xlim([1e3,1e11])
+
+tau=1e-7#np.logspace(-11,-7,120)
+nu=np.logspace(3,12,120)#121.49e6
+delta_sigma_ppm=220*1e-6
+
+tau=1e-11
+rateax.plot(nu,R1_ges(nu*2*np.pi,tau/0.5,1.2e9,delta_sigma_ppm,0.5),label=r'$\tau = 10^{-11}$',lw=2.0,color=c[0])
+rateax.plot(nu,R1_CSA(nu*2*np.pi,tau/0.5,delta_sigma_ppm,0.5),label='CSA',color=c[0],ls='-')
+rateax.plot(nu,R1_DD(nu*2*np.pi,tau/0.5,1.2e9,0.5),label='DD',color=c[0],ls='-.')
+with open('jplot/tau-11-r1ges.dat','w') as fout:
+	for n in nu:
+		fout.write(str(n)+' '+str(R1_ges(nu*2*np.pi,tau/0.5,1.2e9,delta_sigma_ppm,0.5))+'\n')
+with open('jplot/tau-11-r1csa.dat','w') as fout:
+	for n in nu:
+		fout.write(str(n)+' '+str(R1_CSA(nu*2*np.pi,tau/0.5,delta_sigma_ppm,0.5))+'\n')
+with open('jplot/tau-11-r1dd.dat','w') as fout:
+	for n in nu:
+		fout.write(str(n)+' '+str(R1_DD(nu*2*np.pi,tau/0.5,1.2e9,0.5))+'\n')
+
+
+
+#rateax.plot(tau,1./R1_CSA(nu*2*np.pi,tau,delta_sigma_ppm),label='beta=1',color=c[3])
+
+tau=1e-9
+rateax.plot(nu,R1_ges(nu*2*np.pi,tau/0.5,1.2e9,delta_sigma_ppm,0.5),label=r'$\tau = 10^{-9}$',lw=2.0,color=c[2])
+rateax.plot(nu,R1_CSA(nu*2*np.pi,tau/0.5,delta_sigma_ppm,0.5),label='CSA',color=c[2],ls='-')
+rateax.plot(nu,R1_DD(nu*2*np.pi,tau/0.5,1.2e9,0.5),label='DD',color=c[2],ls='-.')
+#rateax.plot(tau,1./R1_CSA(nu*2*np.pi,tau,delta_sigma_ppm),label='beta=1',color=c[3])
+with open('jplot/tau-9-r1ges.dat','w') as fout:
+	for n in nu:
+		fout.write(str(n)+' '+str(R1_ges(nu*2*np.pi,tau/0.5,1.2e9,delta_sigma_ppm,0.5))+'\n')
+with open('jplot/tau-9-r1csa.dat','w') as fout:
+	for n in nu:
+		fout.write(str(n)+' '+str(R1_CSA(nu*2*np.pi,tau/0.5,delta_sigma_ppm,0.5))+'\n')
+with open('jplot/tau-9-r1dd.dat','w') as fout:
+	for n in nu:
+		fout.write(str(n)+' '+str(R1_DD(nu*2*np.pi,tau/0.5,1.2e9,0.5))+'\n')
+
+
+
+tau=1e-8
+rateax.plot(nu,R1_ges(nu*2*np.pi,tau/0.5,1.2e9,delta_sigma_ppm,0.5),label=r'$\tau = 10^{-8}$',lw=2.0,color=c[8])
+rateax.plot(nu,R1_CSA(nu*2*np.pi,tau/0.5,delta_sigma_ppm,0.5),label='CSA',color=c[8],ls='-')
+rateax.plot(nu,R1_DD(nu*2*np.pi,tau/0.5,1.2e9,0.5),label='DD',color=c[8],ls='-.')
+#rateax.plot(tau,1./R1_CSA(nu*2*np.pi,tau,delta_sigma_ppm),label='beta=1',color=c[3])
+with open('jplot/tau-8-r1ges.dat','w') as fout:
+	for n in nu:
+		fout.write(str(n)+' '+str(R1_ges(nu*2*np.pi,tau/0.5,1.2e9,delta_sigma_ppm,0.5))+'\n')
+with open('jplot/tau-8-r1csa.dat','w') as fout:
+	for n in nu:
+		fout.write(str(n)+' '+str(R1_CSA(nu*2*np.pi,tau/0.5,delta_sigma_ppm,0.5))+'\n')
+with open('jplot/tau-8-r1dd.dat','w') as fout:
+	for n in nu:
+		fout.write(str(n)+' '+str(R1_DD(nu*2*np.pi,tau/0.5,1.2e9,0.5))+'\n')
+
+
+
+plt.legend()
+
 i=raw_input('ente')
--- ffc/T1(tau)_csa-vs-DD.py
+++ ffc/T1(tau)_csa-vs-DD.py
@@ -44,18 +44,46 @@ rateax.plot(tau,1./R1_CSA(nu*2*np.pi,tau/0.5,delta_sigma_ppm,0.5),label='CSA',co
 rateax.plot(tau,1./R1_DD(nu*2*np.pi,tau/0.5,1.2e9,0.5),label='DD',color=c[0],ls='-.')
 #rateax.plot(tau,1./R1_CSA(nu*2*np.pi,tau,delta_sigma_ppm),label='beta=1',color=c[3])
 
+with open('jplot2/nu121.49ges.dat','w') as fout:
+	for t in tau:
+		fout.write(str(t)+' '+str(R1_ges(nu*2*np.pi,tau/0.5,1.2e9,delta_sigma_ppm,0.5))+'\n')
+with open('jplot2/nu121.49csa.dat','w') as fout:
+	for t in tau:
+		fout.write(str(t)+' '+str(R1_CSA(nu*2*np.pi,tau/0.5,delta_sigma_ppm,0.5))+'\n')
+with open('jplot2/nu121.49dd.dat','w') as fout:
+	for t in tau:
+		fout.write(str(t)+' '+str(R1_DD(nu*2*np.pi,tau/0.5,1.2e9,0.5))+'\n')
 nu=121.49e5
 rateax.plot(tau,1./R1_ges(nu*2*np.pi,tau/0.5,1.2e9,delta_sigma_ppm,0.5),label=r'$\nu = 12.149 MHz$',lw=2.0,color=c[2])
 rateax.plot(tau,1./R1_CSA(nu*2*np.pi,tau/0.5,delta_sigma_ppm,0.5),label='CSA',color=c[2],ls='-')
 rateax.plot(tau,1./R1_DD(nu*2*np.pi,tau/0.5,1.2e9,0.5),label='DD',color=c[2],ls='-.')
 #rateax.plot(tau,1./R1_CSA(nu*2*np.pi,tau,delta_sigma_ppm),label='beta=1',color=c[3])
 
+with open('jplot2/nu12.149ges.dat','w') as fout:
+	for t in tau:
+		fout.write(str(t)+' '+str(R1_ges(nu*2*np.pi,tau/0.5,1.2e9,delta_sigma_ppm,0.5))+'\n')
+with open('jplot2/nu12.149csa.dat','w') as fout:
+	for t in tau:
+		fout.write(str(t)+' '+str(R1_CSA(nu*2*np.pi,tau/0.5,delta_sigma_ppm,0.5))+'\n')
+with open('jplot2/nu12.149dd.dat','w') as fout:
+	for t in tau:
+		fout.write(str(t)+' '+str(R1_DD(nu*2*np.pi,tau/0.5,1.2e9,0.5))+'\n')
 
 nu=121.49e7
 rateax.plot(tau,1./R1_ges(nu*2*np.pi,tau/0.5,1.2e9,delta_sigma_ppm,0.5),label=r'$\nu = 1.2149 GHz$',lw=2.0,color=c[8])
 rateax.plot(tau,1./R1_CSA(nu*2*np.pi,tau/0.5,delta_sigma_ppm,0.5),label='CSA',color=c[8],ls='-')
 rateax.plot(tau,1./R1_DD(nu*2*np.pi,tau/0.5,1.2e9,0.5),label='DD',color=c[8],ls='-.')
 #rateax.plot(tau,1./R1_CSA(nu*2*np.pi,tau,delta_sigma_ppm),label='beta=1',color=c[3])
+
+with open('jplot2/nu1214.9ges.dat','w') as fout:
+	for t in tau:
+		fout.write(str(t)+' '+str(R1_ges(nu*2*np.pi,tau/0.5,1.2e9,delta_sigma_ppm,0.5))+'\n')
+with open('jplot2/nu1214.9csa.dat','w') as fout:
+	for t in tau:
+		fout.write(str(t)+' '+str(R1_CSA(nu*2*np.pi,tau/0.5,delta_sigma_ppm,0.5))+'\n')
+with open('jplot2/nu1214.9dd.dat','w') as fout:
+	for t in tau:
+		fout.write(str(t)+' '+str(R1_DD(nu*2*np.pi,tau/0.5,1.2e9,0.5))+'\n')
 
 plt.legend()
 
